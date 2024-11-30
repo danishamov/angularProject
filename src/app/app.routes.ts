3501c94bb8d05998f0c01d2534e0a0bc6702c5cc
danishamov <122825234+danishamov@users.noreply.github.com>
2024-11-30 21:59:51 +0000

    Implement details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { CreateComponent } from './user/create/create.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
   //   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
 
   //Start - user routing
 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'create', component: CreateComponent },
+  { path: 'details', component: DetailsComponent },
 ];
