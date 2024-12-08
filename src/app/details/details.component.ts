f45741650010f3029148b3f820031d84868b8b09
danishamov <122825234+danishamov@users.noreply.github.com>
2024-12-08 01:17:47 +0000

    Implement Comment section

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommentComponent } from '../comment/comment.component';
 
 @Component({
   selector: 'app-details',
-  imports: [RouterLink],
+  imports: [RouterLink, CommentComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
